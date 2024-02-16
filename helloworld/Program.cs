fbd7da5ae132cd499b9ce3d5b51c3da71399317a
layton <laytonmatheka@gmail.com>
2024-02-16 17:11:18 +0300

    initial

---
+++ helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, this is my first c# project!");
+
+string name;
+name ="Layton";
+int height;
+height = 180;
+
+Console.WriteLine("My name is " + name + " and I am " + height + "cm tall.");
+
+
+int age = 18;
+
+if (age >= 18)
+{
+    Console.WriteLine("I am an adult.");
+}
+else
+{
+    Console.WriteLine("I am a child.");
+}
